9023073d31e621df824129825c56058887d87db5
eullersa <69916649+eullersa@users.noreply.github.com>
2022-09-23 14:32:50 -0300

    google

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 
 app.get('/', (req, res) => {
     res.send('test')
 })
 
-app.listen(8000, () => {
+app.listen(process.env.PORT, () => {
     console.log('rodando servidor')
 })
